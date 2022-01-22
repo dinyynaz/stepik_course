abbb6ddaafcdeb092aeee396e5171453d053ebd7
DianaM <diny.naz96@gmail.com>
2022-01-23 01:09:23 +0300

    Задание на execute_script

---
+++ test_2_2_5.py
@@ -0,0 +1,44 @@
+import math
+import time
+
+from Tools.scripts.generate_opcode_h import footer
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+
+link = "http://suninjuly.github.io/execute_script.html"
+
+
+def calc(x):
+    return str(math.log(abs(12 * math.sin(int(x)))))
+
+
+try:
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+    x_element = browser.find_element_by_id('input_value')
+    x = x_element.text
+    y = calc(x)
+    y_element = browser.find_element_by_id('answer')
+    y_element.send_keys(y)
+
+    browser.execute_script("window.scrollBy(0, 100);") #скоролл на 100 пикселей
+
+    option1 = browser.find_element_by_css_selector("[type='checkbox']")
+    option1.click()
+
+    option2 = browser.find_element_by_id('robotsRule')
+    option2.click()
+
+    button = browser.find_element_by_xpath("//button[@type='submit']")
+    button.click()
+
+
+
+
+finally:
+    # успеваем скопировать код за 30 секунд
+    time.sleep(10)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
